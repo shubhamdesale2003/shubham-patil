ebe187ba8d81bf7ae9ecb98e2595c251a87e049f
shubhamdesale2003 <shubhamhanumantpatil@gmail.com>
2026-09-07 09:14:57 +0530

    Carousel added

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,16 +1,93 @@
-import React from 'react'
-import '../Styles/projects.scss'
+import React from "react";
+
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import "../Styles/projects.scss";
+
+import project from "../assets/blog.png";
+import portfolio from "../assets/portfolioME.png";
+
 const Project = () => {
   return (
-   <div id="projects">
-    <h2>Projects</h2>
-    <section>
+    <div id="projects">
+      <h2>Projects</h2>
+
+      <section>
         <article>
+          <Carousel
+            showArrows={false}
+            showIndicators={false}
+            showStatus={false}
+            showThumbs={false}
+            interval={2000}
+            infiniteLoop={true}
+            autoPlay={true}
+            stopOnHover={false}
+          >
+            {/* Portfolio */}
+            <div className="projectItem">
+              <img src={portfolio} alt="Personal Portfolio" />
 
+              <aside>
+                <h3>Personal Portfolio</h3>
+
+                <p>
+                  Responsive portfolio website built with React.js and Sass.
+                </p>
+
+                <a
+                  href="https://shubham-desale.vercel.app"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Demo
+                </a>
+
+                {/* <a
+                  href="YOUR_GITHUB_LINK"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a> */}
+              </aside>
+            </div>
+
+            {/* Blog */}
+            <div className="projectItem">
+              <img src={project} alt="MERN Blog" />
+
+              <aside>
+                <h3>MERN Blog</h3>
+
+                <p>
+                  React.js, Sass, Node.js, Express.js, MongoDB, Mongoose,
+                  Cloudinary
+                </p>
+
+                <a
+                  href="https://client-sepia-iota.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Demo
+                </a>
+
+                {/* <a
+                  href="YOUR_GITHUB_LINK"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a> */}
+              </aside>
+            </div>
+          </Carousel>
         </article>
-    </section>
-   </div>
-  )
-}
+      </section>
+    </div>
+  );
+};
 
-export default Project
+export default Project;
